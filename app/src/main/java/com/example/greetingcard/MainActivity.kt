ff932e7ae192c6a9e674ddc2d54d30f3cade5521
Mateusz Matlach <mateusz.matlach@gmail.com>
2022-05-27 21:53:03 +1000

    Update main

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             GreetingCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("World")
+                    Greeting("Ewa")
                 }
             }
         }
